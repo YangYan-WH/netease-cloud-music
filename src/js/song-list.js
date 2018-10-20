25bb7e7d28989c8332f45a1d5a2eabe19b39c728
yangyan <15927288912@126.com>
2018-10-20 21:57:39 +0800

    获取数据库

--- src/js/song-list.js
+++ src/js/song-list.js
@@ -1,43 +1,56 @@
 {
     let view = {
-        el:'#songList-container',
-        template:`
+        el: '#songList-container',
+        template: `
         <ul class="songList">
         </ul>
         `,
-        render(data){
-            let {songs} = data
-            let liList = songs.map((song)=> $('<li></li>').text(song.name))
+        render(data) {
+            let { songs } = data
+            let liList = songs.map((song) => $('<li></li>').text(song.name))
             let $el = $(this.el)
             $(this.el).html(this.template)
             $el.find('ul').empty()
-            liList.map((domLi)=>{
+            liList.map((domLi) => {
                 $el.find('ul').append(domLi)
             })
-            
+
         },
-        clearActive(){
+        clearActive() {
             $(this.el).find('.active').removeClass('active')
         }
     }
-    let model={
-        data:{
-            songs:[ ]
+    let model = {
+        data: {
+            songs: []
+        },
+        find() {
+            var query = new AV.Query('Song');
+            return query.find().then((songs) => {
+                this.data.songs = songs.map(function (song) {
+                    return {id : song.id, ...song.attributes}
+                });
+                console.log(this.data.songs)
+                return songs
+            })
         }
     }
-    let controller={
-        init(view,model){
+    let controller = {
+        init(view, model) {
             this.view = view
             this.model = model
             this.view.render(this.model.data)
-            window.eventHub.on('upload',()=>{
+            window.eventHub.on('upload', () => {
                 this.view.clearActive()
             })
-            window.eventHub.on('create',(songData)=>{
+            window.eventHub.on('create', (songData) => {
                 this.model.data.songs.push(songData)
+                this.view.render(this.model.data)
+            })
+            this.model.find().then(()=>{
                 this.view.render(this.model.data)
             })
         }
     }
-    controller.init(view,model)
+    controller.init(view, model)
 }
